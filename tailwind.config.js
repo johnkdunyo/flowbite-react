fe54df8a33a5797f0bd55b0712c9febee5de5cfa
johnkdunyo <johnkdunyo@gmail.com>
2022-07-14 12:23:21 +0000

    changed indicators colors and control colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}', './node_modules/flowbite/**/*.js'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gold: '#FF9A13',
+        black: '#000000',
+        primaryBlue: '#020B5E',
+        secondaryBlue: '#000C7C',
+        grayBackground: '#f4f4f4',
+      },
+    },
   },
   plugins: [require('flowbite/plugin')],
 };
